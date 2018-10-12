320729693ebfd07372a2d536676f0e857978964b
Administrator <Administrator@sinchon-e-PC>
2018-10-12 13:39:37 +0900

    로그인

--- src/member/MemberDAO.java
+++ src/member/MemberDAO.java
@@ -60,6 +60,44 @@ public class MemberDAO {
 	 }//confirmId() end---------------------------------
 	
 	
+	//-------------------
+		 //회원 가입
+		 //-------------------
+		 public void insertMember(MemberDTO dto) throws Exception{
+			 Connection con=null;
+			 PreparedStatement pstmt=null;
+			 
+			 try{
+				 con=getCon();//커넥션 얻는다
+				 pstmt=con.prepareStatement("insert into member values(?,?,?,?,?,?,?,?,?,?)");
+				 
+				 //?값채우기
+				 pstmt.setString(1, dto.getId());
+				 pstmt.setString(2, dto.getPasswd());
+				 pstmt.setString(3, dto.getName());
+				 pstmt.setString(4, dto.getJumin1());
+				 pstmt.setString(5, dto.getJumin2());
+
+				 pstmt.setString(6, dto.getEmail());
+				 pstmt.setString(7, dto.getZipcode());
+				 pstmt.setString(8, dto.getAddr());
+				 pstmt.setString(9, dto.getJob());
+				 pstmt.setTimestamp(10, dto.getRegdate());
+				 
+				 pstmt.executeUpdate();//쿼리수행*****
+				 
+			 }catch(Exception ex1){
+				 System.out.println("insertMember 예외 : "+ex1);
+			 }finally{
+				 try{
+					 if(pstmt!=null){pstmt.close();}
+					 if(con!=null){con.close();}
+				 }catch(Exception ex2){
+					 
+				 }
+			 }//finally end
+		 }//insertMember() end-----------------------------------
+	 
 	//---------
 	//로그인 (인증)
 	//-----------
